dce3d0da43e4749363b9094f2db30d50c352bec6
Dakota McKay <dakotamckay72@gmail.com>
2025-10-02 08:25:14 -0400

    Add player to main level scene, and write script that allows for player movement.

---
+++ scripts/player.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+
+func _physics_process(delta: float) -> void:
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = 300 * horizontal_direction
+	move_and_slide()
