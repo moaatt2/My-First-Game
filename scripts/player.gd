c2700a3988d619c779e0e4907b628f59365377d5
Dakota McKay <dakotamckay72@gmail.com>
2025-10-02 08:26:44 -0400

    Allow for speed to be controlled via the inspector instead of hard coded in script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
+@export var speed = 300
 
 func _physics_process(delta: float) -> void:
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	velocity.x = 300 * horizontal_direction
+	velocity.x = speed * horizontal_direction
 	move_and_slide()
