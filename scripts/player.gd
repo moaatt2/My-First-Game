b14ca2d3ad1b191e7aaeff51b4099a1a7edf5d49
Dakota McKay <dakotamckay72@gmail.com>
2025-10-03 08:09:37 -0400

    Add falling physics to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 @export var speed = 300
+@export var gravity = 30
 
 func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y = min(velocity.y + gravity, 500)
+	
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	velocity.x = speed * horizontal_direction
 	move_and_slide()
+	
+	print(velocity)
