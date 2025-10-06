c26814ec7795da3b642ad203095d27c95604f169
Dakota McKay <dakotamckay72@gmail.com>
2025-10-06 08:12:20 -0400

    Add a dash.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var gravity = 30
 @export var jump_force = 300
 
+var DASH_SPEED = 4
+var is_dashing = false
 
 func _physics_process(delta: float) -> void:
 	# Falling logic
@@ -16,7 +18,23 @@ func _physics_process(delta: float) -> void:
 
 	# Movement logic
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	velocity.x = speed * horizontal_direction
+
+	if Input.is_action_just_pressed("dash"):
+		if !is_dashing and horizontal_direction:
+			start_dash()
+
+	if is_dashing:
+		velocity.x = speed * DASH_SPEED * horizontal_direction
+	else:
+		velocity.x = speed * horizontal_direction
 	move_and_slide()
 	
 	print(velocity)
+
+func start_dash():
+	is_dashing = true
+	$DashTimer.connect("timeout", stop_dash)
+	$DashTimer.start()
+
+func stop_dash():
+	is_dashing = false
