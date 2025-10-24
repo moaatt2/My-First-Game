d0a2e0b0e2aed5c19e8cdfbc2baf38757c9f23fc
Dakota McKay <dakotamckay72@gmail.com>
2025-10-24 08:03:21 -0400

    Fix error that occurs on last level.

--- scripts/exit_door.gd
+++ scripts/exit_door.gd
@@ -23,9 +23,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		
 		# Get index of current file
 		var current_index = files.find(current_scene_name)
-		if current_index != -1 and current_index <= files.size():
-			print(dir_path)
-			print(files)
-			print(files[current_index + 1])
+		if current_index + 1 < files.size():
+			print("Going to next level")
 			var next_scene_path = dir_path + "/" + files[current_index + 1]
 			get_tree().change_scene_to_file(next_scene_path)
+		else:
+			print("No next level")
