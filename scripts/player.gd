484ac035d482695acbec637767a11eb26ad53a83
Dakota McKay <dakotamckay72@gmail.com>
2025-10-30 08:23:40 -0400

    Replace dedicated jump/dash with action button using picked up abilities as resources.

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,29 +6,35 @@ extends CharacterBody2D
 
 var DASH_SPEED = 4
 var is_dashing = false
+var ability_queue = []
 
 func _physics_process(delta: float) -> void:
 	# Falling logic
 	if !is_on_floor():
 		velocity.y = min(velocity.y + gravity, 500)
 
-	# Jump logic
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -jump_force
-
 	# Movement logic
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 
-	if Input.is_action_just_pressed("dash"):
-		if !is_dashing and horizontal_direction:
-			start_dash()
+	# Handle action
+	if Input.is_action_just_pressed("action"):
+		if ability_queue.size() > 0:
+			var action = ability_queue.pop_front()
+			if action == "jump":
+				velocity.y = -jump_force
+			elif action == "dash":
+				if !is_dashing and horizontal_direction:
+					start_dash()
+			else:
+				print("Unknown Action: " + str(action))
 
+	# Handle Dash
 	if is_dashing:
 		velocity.x = speed * DASH_SPEED * horizontal_direction
 	else:
 		velocity.x = speed * horizontal_direction
 	move_and_slide()
-	
+
 	print(velocity)
 
 func start_dash():
@@ -40,4 +46,5 @@ func stop_dash():
 	is_dashing = false
 
 func add_charge(ability) -> void:
-	print("Adding Charge")
+	ability_queue.append(ability)
+	print(ability_queue)
