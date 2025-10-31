8b10dc40c01e3fb618caf58e3a15a15c60bf920c
Dakota McKay <dakotamckay72@gmail.com>
2025-10-31 08:18:32 -0400

    Add co-ordinates of ability to ability queue.

--- scripts/ability_dash.gd
+++ scripts/ability_dash.gd
@@ -3,5 +3,7 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Ran into dash")
 	if body.has_method("add_charge"):
-		body.add_charge("dash")
+		var x = body.position.x
+		var y = body.position.y
+		body.add_charge(["dash", x, y])
 		queue_free()
--- scripts/ability_jump.gd
+++ scripts/ability_jump.gd
@@ -4,5 +4,7 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Ran into jump")
 	if body.has_method("add_charge"):
-		body.add_charge("jump")
+		var x = body.position.x
+		var y = body.position.y
+		body.add_charge(["jump", x, y])
 		queue_free()
