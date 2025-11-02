e762943dba85e379a8dd1a1de87c1d56d8d7a175
Dakota McKay <dakotamckay72@gmail.com>
2025-11-01 20:55:15 -0400

    Pass ability position to add charge instead of player position.

--- scripts/ability_dash.gd
+++ scripts/ability_dash.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Ran into dash")
 	if body.has_method("add_charge"):
-		var x = body.position.x
-		var y = body.position.y
+		var x = self.position.x
+		var y = self.position.y
 		body.add_charge(["dash", x, y])
 		queue_free()
--- scripts/ability_jump.gd
+++ scripts/ability_jump.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Ran into jump")
 	if body.has_method("add_charge"):
-		var x = body.position.x
-		var y = body.position.y
+		var x = self.position.x
+		var y = self.position.y
 		body.add_charge(["jump", x, y])
 		queue_free()
