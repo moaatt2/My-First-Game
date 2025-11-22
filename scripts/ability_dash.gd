c67d77f9f705f33e77415a500c401545a9aed413
Dakota McKay <dakotamckay72@gmail.com>
2025-11-22 17:30:28 -0500

    Clear irrelevant info from branch.

--- scripts/ability_dash.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Ran into dash")
-	if body.has_method("add_charge"):
-		var x = self.position.x
-		var y = self.position.y
-		body.add_charge(["dash", x, y])
-		queue_free()
